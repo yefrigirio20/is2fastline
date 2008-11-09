2204897f1d5a0defec4ddf88030c848e7327aa4c
jpfuturih <jpfuturih@310f4e14-a2a9-11dd-a9c7-49dec45ca499>
2008-11-09 01:38:37 +0000

    ya se puede aumentar google Earth

--- fastlinePHP/web/contra.php
+++ fastlinePHP/web/contra.php
@@ -14,13 +14,13 @@ window.history.back();
 </script>
 </head>
 <?php
-$c="fastline";
-if($c==$valor)
-{echo "<center><BR><BR><BR><img src="../imagenes/feliz.jpg" width="112" height="107">Contraseña correcta<br></center>";
+$c="itachi";
+if($c==$contrasena)
+{echo "<center><BR><BR><BR><img src='../imagenes/feliz.jpg' width='112' height='107'>Contraseña correcta<br></center>";
  echo "<center><input name=back type=button value=Ingresar onClick=abrir(); ></center>";
 }
 else
-{echo "<center><BR><BR><BR><img src="../imagenes/triste.jpg" width="145" height="125">Contraseña incorrecta <BR></center>";
+{echo "<center><BR><BR><BR><img src='../imagenes/triste.jpg' width='145' height='125'>Contraseña incorrecta <BR></center>";
  echo "<center>Si desea intentelo de nuevo<BR></center>";
  echo "<center><input name=back type=button value=Atras onClick=atras(); ></center>";
  }
